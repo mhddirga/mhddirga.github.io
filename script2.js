9c6f593d6211719f77fa221f0678281c60516aad
mdwikidestian <m.dwiki.destian@mail.ugm.ac.id>
2022-04-16 21:17:52 +0700

    10

---
+++ script2.js
@@ -0,0 +1,11 @@
+document.getElementById("medium_button").addEventListener('click', function() {
+    window.open("https://medium.com/@mdwikidestian", '_blank').focus();
+});
+
+document.getElementById("dribble_button").addEventListener('click', function() {
+    window.open("https://dribble.com/mdwikidestian", '_blank').focus();
+});
+
+document.getElementById("behance_button").addEventListener('click', function() {
+    window.open("https: //www.behance.net/dwikidestian", '_blank').focus();
+});
